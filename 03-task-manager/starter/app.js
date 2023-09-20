ba622df692de731c9cedb93dc93fd2dc6049de38
Gustavo Aleman Sanchez <blacksubmarine.k@gmail.com>
2023-09-20 16:15:43 -0600

    first app

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,21 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+//routes
+
+//middleware
+app.use(express.json())
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+});
+
+app.use('/api/v1/tasks', tasks)
+
+const port = 3000
+
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
+
+
+
